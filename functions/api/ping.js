5afefa5fda0bfd002ca969b0be694cedc10ffc19
ZveenTV <zveentv@gmail.com>
2026-08-08 19:58:19 +0200

    Add Cloudflare API health check

---
+++ functions/api/ping.js
@@ -0,0 +1,1 @@
+export function onRequestGet(){return new Response(JSON.stringify({ok:true,service:'cloudflare-pages-functions'}),{status:200,headers:{'Content-Type':'application/json; charset=utf-8','Cache-Control':'no-store'}})}
